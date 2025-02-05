d4355d752e5da80375b7005f7c7e470873c3cf85
CalsXshree <maharjanraj89@gmail.com>
2025-02-05 15:32:29 +0545

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marshall Photographs</title>
+</head>
+
+<body>
+    <div class="border border-green-950">
+        <div class="flex justify-between items-center bg-green-500 h-[58px] ">
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About </a></li>
+                    <li><a href="">Portfolio </a></li>
+                    <li><a href="">Experience </a></li>
+                    <li><a href="">Journal </a></li>
+                    <li><a href="">Contact </a></li>
+                </ul>
+            </nav>
+            <div>
+                <a href="/joinus"
+                    class="font-extrabold border border-white bg-black hover:bg-green-800 text-white px-4 py-2 rounded">Book
+                    Now</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
