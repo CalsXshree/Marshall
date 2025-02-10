4d27542b996c3c07f6de4a1dadb213f04a509e2c
raj <maharjanraj89@gmail.com>
2025-02-10 16:51:02 +0545

    galleries added

--- resources/views/components/portfolio.blade.php
+++ resources/views/components/portfolio.blade.php
@@ -11,35 +11,55 @@
             </div>
 
         </h2>
+
+        <div class="uppercase text-black text-center font-light text-2xl">discover the stories</div>
     </div>
-    <div class="uppercase text-black text-center font-light text-2xl">discover the stories</div>
-</div>
-<div class="swiper mySwiper max-w-4xl mx-auto mt-8 shadow-lg rounded-lg overflow-hidden">
-    <div class="swiper mySwiper">
-        <div class="swiper-wrapper">
-            <!-- Slide 1 -->
-            <div class="swiper-slide">
-                <img src="{{ asset('images/marshall(34).jpg') }}" alt="Image 1" class="w-full h-auto">
+    <div class="swiper mySwiper max-w-4xl mx-auto mt-8 shadow-lg rounded-lg overflow-hidden">
+        <div class="swiper mySwiper">
+            <div class="swiper-wrapper">
+                <!-- Slide 1 -->
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/marshall(34).jpg') }}" alt="Image 1" class="w-full h-auto">
+                </div>
+                <!-- Slide 2 -->
+                <div class="swiper-slide">
+                    <img src="a{asset('images/marshall(33).jpg')}" alt="Image 2" class="w-full h-auto">
+                </div>
+                <!-- Slide 3 -->
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/marshall(30).jpg') }}" alt="Image 3" class="w-full h-auto">
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/marshall(31).jpg') }}" alt="Image 3" class="w-full h-auto">
+                </div>
+                <div class="swiper-slide">
+                    <img src="{{ asset('images/marshall(32).jpg') }}" alt="Image 3" class="w-full h-auto">
+                </div>
             </div>
-            <!-- Slide 2 -->
-            <div class="swiper-slide">
-                <img src="a{asset('images/marshall(33).jpg')}" alt="Image 2" class="w-full h-auto">
-            </div>
-            <!-- Slide 3 -->
-            <div class="swiper-slide">
-                <img src="{{ asset('images/marshall(30).jpg') }}" alt="Image 3" class="w-full h-auto">
-            </div>
-            <div class="swiper-slide">
-                <img src="{{ asset('images/marshall(31).jpg') }}" alt="Image 3" class="w-full h-auto">
-            </div>
-            <div class="swiper-slide">
-                <img src="{{ asset('images/marshall(32).jpg') }}" alt="Image 3" class="w-full h-auto">
-            </div>
+            <!-- Pagination -->
+            <div class="swiper-pagination"></div>
+            <!-- Navigation Buttons -->
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
         </div>
-        <!-- Pagination -->
-        <div class="swiper-pagination"></div>
-        <!-- Navigation Buttons -->
-        <div class="swiper-button-prev"></div>
-        <div class="swiper-button-next"></div>
+        <div class="font-serif text-center p-1 text-xl bg-[#ffffff] text-[#1e1f1b] "">
+            <i> "Marshall's ability to
+                capture
+                raw
+                emotion, fleeting moments, and genuine connections is truly unmatched.
+                Despite the unexpected downpour, his calm presence and creative vision turned the day into something
+                magical.
+                The photos we received aren’t just images—they are emotions frozen in time,filled with warmth, love,
+                and
+                authenticity.
+                Every detail, every smile tells a story, and we couldn’t be more grateful for the memories
+                he
+                created for us."<br><br>-Ram and Sita</i>
+        </div>
+        <div class="text-center mb-5 mt-5">
+            <a href="" class="border border-black py-2 px-8 uppercase hover:bg-gray-700 hover:text-white">
+                View galleries
+            </a>
+        </div>
     </div>
 </div>
